95939f744f0afcdc7ab63ab1a03ac638a6a47c74
umtazazi <umutazazi3@gmail.com>
2023-12-17 14:18:02 +0300

    new file:   src/screens/LoginScreen.js 	new file:   src/screens/RegisterScreen.js

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+
+
+
+const LoginScreen = () => {
+    return (
+        <View>
+            <Text>Login Page</Text>
+        </View>
+    );
+}
+
+
+
+export default LoginScreen;
